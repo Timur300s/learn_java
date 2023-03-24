e72e814194d48738e3f0c21e9d54e672392e88ee
Timur <fattahovtimur259#gmail.com>
2023-03-24 18:56:25 +0500

    add lesson2-check

--- src/stepik/algorithms/first/lesson1.java
+++ src/stepik/algorithms/first/lesson1.java
@@ -1,7 +1,25 @@
 package stepik.algorithms.first;
 
+import java.util.Scanner;
+
 public class lesson1 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите число ");
+        int n = scanner.nextInt();
+        int[] f = new int[n+1];
+        f[0]=0;
+        f[1]=1;
+        for (int i=2;i<=n;i++){
+            f[i]=f[i-1]+f[i-2];
+        }
+        System.out.println(f[n]);
+
+
+
+
+
+
+
     }
 }
--- src/stepik/algorithms/first/lesson2.java
+++ src/stepik/algorithms/first/lesson2.java
@@ -1,7 +1,10 @@
 package stepik.algorithms.first;
 
+import java.util.Scanner;
+
 public class lesson2 {
     public static void main(String[] args) {
         System.out.println("check git push");
+
     }
 }
