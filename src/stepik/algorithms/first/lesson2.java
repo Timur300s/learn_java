961be95fe5cfe882d4c93c8daba0aeac68333e63
Timur <fattahovtimur259#gmail.com>
2023-03-26 16:16:32 +0500

    add lesson2- 2 setting the Fibonacci number

--- src/stepik/algorithms/first/lesson2.java
+++ src/stepik/algorithms/first/lesson2.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 
 public class lesson2 {
     public static void main(String[] args) {
-        System.out.println("check git push");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] f = new int[n+1];
+        f[0]=0;
+        f[1]=1;
+        for (int i=2;i<=n;i++){
+            f[i]=(f[i-1]+ f[i-2])%10;
+        }
+        System.out.println(f[n]);
 
     }
 }
